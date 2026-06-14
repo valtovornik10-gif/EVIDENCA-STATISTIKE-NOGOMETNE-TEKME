9a1bdb39a6eff1887b32ec2670a52913e52c053d
valtovornik10-gif <val.tovornik10@gmail.com>
2026-06-14 17:20:38 +0200

    Create reset.php

---
+++ reset.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+$_SESSION['gol1'] = 0;
+$_SESSION['gol2'] = 0;
+
+$_SESSION['dogodki1'] = [];
+$_SESSION['dogodki2'] = [];
+
+unset($_SESSION['zacetek']);
+unset($_SESSION['konec']);
+unset($_SESSION['tekma_id']);
+
+unset($_SESSION['izloceni']);
+unset($_SESSION['izven_igre']);
+unset($_SESSION['notri_v_igri']);
+
+unset($_SESSION['zadnja_minuta']);
+
+header("Location:index.php");
+exit;
+?>
