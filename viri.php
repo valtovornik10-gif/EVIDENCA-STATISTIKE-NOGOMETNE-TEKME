8b3e1e834fc3308ac0da837310c5752fce0cf134
valtovornik10-gif <val.tovornik10@gmail.com>
2026-06-14 17:24:19 +0200

    Create viri.php

---
+++ viri.php
@@ -0,0 +1,10 @@
+<h1>Viri</h1>
+
+<ul>
+    <li><a href="https://www.w3schools.com/css/css3_box-sizing.asp">W3schools</a></li>
+    <li><a href="https://dev.mysql.com/doc">MYSQL</a></li>
+    <li><a href="https://www.w3schools.com/php">W3</a></li>
+    <li><a href="https://developer.mozilla.org">Developer</a></li>
+</ul>
+
+<a href="shrani.php">Nazaj</a>
