46acac3946826706a580ec73943cc68294d0582a
valtovornik10-gif <val.tovornik10@gmail.com>
2026-06-16 17:35:50 +0200

    Update baza.php

--- baza.php
+++ baza.php
@@ -1,10 +1,10 @@
 <?php
 
 $mysqli = new mysqli(
-    "localhost",
-    "root",
-    "",
-    "statistika"
+    "sql204.infinityfree.com",
+    "if0_42007777",
+    "D49VHU8KmL4Gj",
+    "if0_42007777_statistike"
 );
 
 if($mysqli->connect_error){
