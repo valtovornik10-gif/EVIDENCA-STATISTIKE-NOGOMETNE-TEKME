f6bb17f1b18e879aebd16868b9044e7ca6291fa5
valtovornik10-gif <val.tovornik10@gmail.com>
2026-06-18 18:35:10 +0200

    Update viri.php

--- viri.php
+++ viri.php
@@ -2,7 +2,7 @@
 
 <ul>
     <li><a href="https://www.w3schools.com/css/css3_box-sizing.asp">W3schools</a></li>
-    <li><a href="https://www.w3schools.com/php/func_var_unset.asp">W3schools part2.</a></li>
+    <li><a href="https://www.w3schools.com/php/func_var_unset.asp">unset</a></li>
     <li><a href="https://www.w3schools.com/php">W3</a></li>
     
 </ul>
