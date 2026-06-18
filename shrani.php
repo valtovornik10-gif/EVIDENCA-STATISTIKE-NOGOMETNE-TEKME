099afbf9c8d7db215c74aeb75520b10be1c37e43
valtovornik10-gif <val.tovornik10@gmail.com>
2026-06-18 18:26:57 +0200

    Update shrani.php

--- shrani.php
+++ shrani.php
@@ -1,11 +1,18 @@
 <?php
 session_start();
+
+if(!isset($_SESSION['konec'])){
+    header("Location: konec.php");
+    exit();
+}
+include "baza.php";
 ?>
 
 <!DOCTYPE html>
 <html lang="sl">
 <head>
     <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Analiza tekme</title>
     <link rel="stylesheet" href="glavna.css">
 </head>
