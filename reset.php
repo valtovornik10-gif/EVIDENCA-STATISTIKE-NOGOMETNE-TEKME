72f7c0102e84d5809d3418ee24e5ae8c170ce31f
valtovornik10-gif <val.tovornik10@gmail.com>
2026-06-18 18:29:34 +0200

    Update reset.php

--- reset.php
+++ reset.php
@@ -14,6 +14,7 @@ unset($_SESSION['tekma_id']);
 unset($_SESSION['izloceni']);
 unset($_SESSION['izven_igre']);
 unset($_SESSION['notri_v_igri']);
+unset($_SESSION['ze_vstopili']);
 
 unset($_SESSION['zadnja_minuta']);
 
